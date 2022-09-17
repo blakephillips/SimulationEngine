7c80fa68d5f8ae433b475d684e95821b760abceb
Blake Phillips <blake.a.phillips3@gmail.com>
2022-09-17 18:45:53 -0400

    Start object building

---
+++ core/src/com/blakephillips/engine/utilities/grid/Chunk.java
@@ -0,0 +1,12 @@
+package com.blakephillips.engine.utilities.grid;
+
+import java.util.List;
+
+public class Chunk {
+    public int id;
+
+    public Chunk(int id) {
+        this.id = id;
+    }
+
+}
